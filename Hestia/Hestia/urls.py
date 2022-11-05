9dd5ebebc226aec8500adb974144e0fc93b4007c
Radoslav888 <kolioficheto132@gmail.com>
2022-11-05 16:34:41 +0200

    Created project

--- Hestia/Hestia/urls.py
+++ Hestia/Hestia/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('bulgarian_real_estate.common.urls'))
+    path('', include('Hestia.common.urls'))
 ]
--- bulgarian_real_estate/bulgarian_real_estate/common/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from bulgarian_real_estate.common.views import index
-
-urlpatterns = [
-    path('', index, name='index'),
-]
